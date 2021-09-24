86b23449b621cc9cfc79ce1e903d31fd1de84629
Rodo TuBot <84582248+TuOdoo@users.noreply.github.com>
2021-09-24 14:20:58 -0500

    Módulo con errores
    Módulo con errores

--- website_smartarget_whatsapp_contact_us/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2021 Smartarget <https://smartarget.onkune>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import website
-from . import website_config_settings
