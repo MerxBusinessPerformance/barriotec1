f4822276f0089cd03b0ab5799fef8d9736c850f3
Rafael Ramirez <rafa.yael@gmail.com>
2021-12-14 16:40:41 -0600

    fix: #30 fecha de salida agregada

--- website_booking_system/models/product_template.py
+++ website_booking_system/models/product_template.py
@@ -76,6 +76,7 @@ class SaleOrderLine(models.Model):
 
     booking_slot_id = fields.Many2one("booking.slot", string="Booking Slot")
     booking_date = fields.Date(string="Booking Date")
+    booking_date_out = fields.Date(string="Booking Out Date")
     booked_slot_id = fields.Many2one(related="booking_slot_id.time_slot_id", string="Booked Slot", store=True)
     booked_plan_id = fields.Many2one(related="booking_slot_id.plan_id", string="Booked Plan", store=True)
     booking_plan_line_id = fields.Many2one(
